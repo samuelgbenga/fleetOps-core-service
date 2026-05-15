5122f4b1429d5921fa8d4e5c0afdba615b650db1
Agholordavid19 <agholordavid19@gmail.com>
2026-05-15 06:43:24 +0100

    test

--- src/main/java/com/fleetops/core/config/SecurityConfig.java
+++ src/main/java/com/fleetops/core/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+//        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
