0750609c13decd2e1eedae7465c1ba660c26cd55
Samuel Joseph <samuel.joseph@whytelabeltech.com>
2026-05-23 12:50:07 +0100

    added cors policy

--- src/main/java/com/fleetops/core/shared/config/SecurityConfig.java
+++ src/main/java/com/fleetops/core/shared/config/SecurityConfig.java
@@ -63,9 +63,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
 
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
